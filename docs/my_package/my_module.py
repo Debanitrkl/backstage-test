f8467d9caf923d7be1fa31ce10de0d3d0dd2bff7
Debabrata Panigrahi <50622005+Debanitrkl@users.noreply.github.com>
2024-11-26 10:24:08 +0530

    Create my_module.py

---
+++ docs/my_package/my_module.py
@@ -0,0 +1,13 @@
+# my_package/my_module.py
+
+def greet(name: str) -> str:
+    """
+    Greets the user.
+
+    Args:
+        name (str): Name of the user.
+
+    Returns:
+        str: A greeting message.
+    """
+    return f"Hello, {name}!"
